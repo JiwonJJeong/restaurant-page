2f3ed010e4419b98879005255d6f10db3ae2daaa
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2024-06-10 12:58:25 -0700

    Add tab switching logic to buttons

--- src/index.js
+++ src/index.js
@@ -6,23 +6,61 @@ import {aboutPage} from './about.js';
 
 const pageManager = function(){
 
+    let currentPage = null;
     const init = function(){
         cacheDom();
         bindEvents();
+        renderHomePage();
     }
 
     let contentSection;
+    let homeButton;
+    let menuButton;
+    let aboutButton;
     const cacheDom = function(){
         contentSection = document.querySelector("#content");
-    }
-    
-    const renderHomePage = function(){
-        contentSection.appendChild(homePage.createHomePage());
+        homeButton = document.querySelector(".home.button");
+        menuButton = document.querySelector(".menu.button");
+        aboutButton = document.querySelector(".about.button");
     }
 
     const bindEvents = function(){
-        //bind events
+        homeButton.addEventListener("click", renderHomePage);
+        menuButton.addEventListener("click", renderMenuPage);
+        aboutButton.addEventListener("click", renderAboutPage);
     }
+
+    const renderHomePage = function(){
+        if (currentPage !== "home"){
+            deletePageContent();
+            contentSection.appendChild(homePage.createHomePage());
+        }
+        currentPage = "home";
+    }
+
+    const renderMenuPage = function(){
+        if (currentPage !== "menu"){
+            deletePageContent();
+            contentSection.appendChild(menuPage.createMenuPage());
+        }
+        currentPage ="menu";
+    }
+
+    const renderAboutPage = function(){
+        if (currentPage != "about"){
+            deletePageContent();
+            contentSection.appendChild(aboutPage.createAboutPage());
+        }
+        currentPage = "about";
+    }
+
+    const deletePageContent = function(){
+        while (contentSection.firstChild){
+            contentSection.removeChild(contentSection.firstChild);
+        }
+    }
+
+
 
     return {init,renderHomePage};
 }();
